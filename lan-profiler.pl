a66cf1b13e61456edd98548d6f6f4322d707055c
Francesco Ruvolo <ruvolof@gmail.com>
2013-10-19 20:46:46 +0200

    ping lan implemented

--- lan-profiler.pl
+++ lan-profiler.pl
@@ -2,15 +2,60 @@
 
 use strict;
 use warnings;
+use Time::HiRes;
 use lib 'inc';
 use IO::Interface::Simple;
+use Net::Netmask;
+use Net::Ping;
 
 my @if_list = IO::Interface::Simple->interfaces;
+my $ping = Net::Ping->new('tcp', 0.05);
+$ping->hires();
 
-print 'Available interfaces: ';
-
-for my $if (@if_list) {
-	print $if, ' ';
+if (scalar(@if_list) => 1) {
+	print "Available interfaces:\n";
+	for my $if (@if_list) {
+		print "\t$if (", $if->address, ")\n" if $if->address;
+	}
+	print "\n";
+}
+else {
+	print "No interface found. Exiting.\n";
+	exit 0;
 }
 
-print "\n";
+for my $if (@if_list) {
+	# Skipping loopback interface
+	if ($if eq 'lo') {
+		print "Skipping loopback interface.\n\n";
+		next;
+	}
+	# Skipping VirtualBox virtual interfaces
+	elsif ($if =~ m/vbox/) {
+		print "Skipping VirtualBox virtual interfaces.\n\n";
+		next;
+	}
+	
+	print "Scanning $if.\n";
+	my $address = $if->address;
+	my $netmask = $if->netmask;
+	my @lan;
+	
+	my $block = new2 Net::Netmask ($address, $netmask);
+	
+	if (defined $block) {
+		@lan = $block->enumerate;
+	}
+	else {
+		print "Invalid netmask for $if. Skipping.\n";
+		next;
+	}
+	
+	for my $host (@lan) {
+		print "$host is alive.\n" if $ping->ping($host);
+	}
+	
+	print "\n";
+}
+
+exit 0;

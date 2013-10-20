8912598fa30d8c23dc256dca959c26ee8dfc6bef
Francesco Ruvolo <ruvolof@gmail.com>
2013-10-20 18:01:47 +0200

    vendor name now showed

--- lan-profiler.pl
+++ lan-profiler.pl
@@ -4,12 +4,59 @@
 use strict;
 use warnings;
 use Time::HiRes;
+use IO::Handle;
 use lib 'inc';
 
 # From CPAN in 'inc' subfolder
 use IO::Interface::Simple;
 use Net::Netmask;
 use Net::Ping;
+use HTTP::Tiny;
+
+# Setting autoflush for STDOUT
+autoflush STDOUT 1;
+
+sub extract_oui_from_html {
+	my $html = shift;
+	my $lookup_mac = shift;
+
+	$html =~ s/<pre>.*?$lookup_mac/<pre>$lookup_mac/is;
+
+	# sometimes the HTML returns more than one OUI because
+	# IEEE has a problem parsing their own data when they
+	# have private blocks
+	my( $ouis ) = $html =~ m|<pre>(.*?)</pre>|gs;
+	return unless defined $ouis;
+	$ouis =~ s/<\/?b>//gs; # remove bold around the OUI
+
+	my @entries = split /\n\s*\n/, $ouis;
+	return unless defined $entries[0];
+	return $entries[0] unless defined $entries[1];
+
+	my $result = $entries[0];
+
+	foreach my $entry ( @entries ) {
+		$entry =~ s/^\s+|\s+$//;
+		my $found_mac = normalize_mac( substr $entry, 0, 8 );
+		if( $found_mac eq $lookup_mac ) {
+			$result = $entry;
+			last;
+			}
+		}
+
+	return $result;
+}
+
+sub parse_oui {
+	my $oui = shift;
+	return [] unless $oui;
+
+	my @lines = map { s/^\s+//; $_ ? $_ : () } split /$/m, $oui;
+	splice @lines, 1, 1, ();
+
+	$lines[0] =~ s/\S+\s+\S+\s+//;
+	return \@lines;
+}
 
 my @if_list = IO::Interface::Simple->interfaces;
 my $ping = Net::Ping->new('tcp', 0.05);
@@ -61,6 +108,15 @@ for my $if (@if_list) {
 			if ($arp_cache !~ m/no match found/) {
 				my $mac = (split(' ', $arp_cache))[3];
 				print "\t", $mac;
+				my $vendor_id = substr $mac, 0, 8;
+				$vendor_id =~ tr/:/-/;
+				my $response = HTTP::Tiny->new->get("http://standards.ieee.org/cgi-bin/ouisearch?$vendor_id");
+				if (length $response->{content}) {
+					my $vendor = parse_oui(extract_oui_from_html($response->{content}, $vendor_id));
+					if ($vendor) {
+						print "\t", $vendor->[0];
+					}
+				}
 			}
 			print "\n";
 		}

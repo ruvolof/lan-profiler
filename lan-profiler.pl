48d6df2c1f9184a011f0b631b0edc761f59de7d9
Francesco Ruvolo <ruvolof@gmail.com>
2015-12-21 00:36:54 +0100

    Now using API from macvendors.com, varius small fixes

--- lan-profiler.pl
+++ lan-profiler.pl
@@ -16,54 +16,12 @@ use HTTP::Tiny;
 autoflush STDOUT 1;
 autoflush STDERR 1;
 
-##
-# These functions have been taken from Net::MAC::Vendor by
-# Brian D Foy.
-##
-sub extract_oui_from_html {
-	my $html = shift;
-	my $lookup_mac = shift;
-
-	$html =~ s/<pre>.*?$lookup_mac/<pre>$lookup_mac/is;
-
-	# sometimes the HTML returns more than one OUI because
-	# IEEE has a problem parsing their own data when they
-	# have private blocks
-	my( $ouis ) = $html =~ m|<pre>(.*?)</pre>|gs;
-	return unless defined $ouis;
-	$ouis =~ s/<\/?b>//gs; # remove bold around the OUI
-
-	my @entries = split /\n\s*\n/, $ouis;
-	return unless defined $entries[0];
-	return $entries[0] unless defined $entries[1];
-
-	my $result = $entries[0];
-
-	foreach my $entry ( @entries ) {
-		$entry =~ s/^\s+|\s+$//;
-		my $found_mac = normalize_mac( substr $entry, 0, 8 );
-		if( $found_mac eq $lookup_mac ) {
-			$result = $entry;
-			last;
-			}
-		}
-
-	return $result;
+sub getVendor {
+	my $mac = $_[0];
+	my $response = HTTP::Tiny->new->get('http://api.macvendors.com/' . $mac);
+	
+	return $response->{content};
 }
-
-sub parse_oui {
-	my $oui = shift;
-	return [] unless $oui;
-
-	my @lines = map { s/^\s+//; $_ ? $_ : () } split /$/m, $oui;
-	splice @lines, 1, 1, ();
-
-	$lines[0] =~ s/\S+\s+\S+\s+//;
-	return \@lines;
-}
-##
-# End of code taken from Net::Mac::Vendor.
-##
 
 # Retrieving list of interfaces
 my @if_list = (`/sbin/ifconfig -a` =~ m/^(\w+)\b/mg);
@@ -74,8 +32,8 @@ for my $if (@if_list) {
 	my $new_if = {};
 	my $if_info = `/sbin/ifconfig $if`;
 	$new_if->{name} = $if;
-	($new_if->{address}) = ($if_info =~ m/inet addr:(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})\b/);
-	($new_if->{netmask}) = $if_info =~ m/Mask:(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})\b/;
+	($new_if->{address}) = ($if_info =~ m/inet\s(?:addr:){0,1}(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})\b/);
+	($new_if->{netmask}) = $if_info =~ m/(?:Mask:|netmask\s)(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})\b/;
 	push @if_h, $new_if;
 }
 
@@ -117,7 +75,7 @@ for my $if (@if_h) {
 		@lan = $block->enumerate;
 	}
 	else {
-		print "Invalid netmask for $if. Skipping.\n";
+		print "Invalid netmask for $if->{name}. Skipping.\n";
 		next;
 	}
 	
@@ -141,15 +99,8 @@ for my $if (@if_h) {
 			if ($arp_cache !~ m/no match found/) {
 				my $mac = (split(' ', $arp_cache))[3];
 				print "\t", $mac;
-				my $vendor_id = substr $mac, 0, 8;
-				$vendor_id =~ tr/:/-/;
-				my $response = HTTP::Tiny->new->get("http://standards.ieee.org/cgi-bin/ouisearch?$vendor_id");
-				if (length $response->{content}) {
-					my $vendor = parse_oui(extract_oui_from_html($response->{content}, $vendor_id));
-					if ($vendor) {
-						print "\t", $vendor->[0];
-					}
-				}
+				my $vendor = getVendor($mac);
+				print "\t", $vendor;
 			}
 			print "\n";
 		}

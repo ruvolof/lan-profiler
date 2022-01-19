33df6436268c94ce00779e57b49bff6c9a2e0575
Francesco Ruvolo <ruvolof@gmail.com>
2022-01-19 21:42:20 +0100

    Updated to work with ip rather than ifconfig. Improved code readability.

--- lan-profiler.pl
+++ lan-profiler.pl
@@ -1,13 +1,11 @@
 #!/usr/bin/env perl
 
-# Core modules
 use strict;
 use warnings;
 use Time::HiRes;
 use IO::Handle;
 use lib 'inc';
 
-# From CPAN in 'inc' subfolder
 use Net::Netmask;
 use Net::Ping;
 use HTTP::Tiny;
@@ -19,86 +17,97 @@ autoflush STDERR 1;
 sub getVendor {
 	my $mac = $_[0];
 	my $response = HTTP::Tiny->new->get('http://api.macvendors.com/' . $mac);
-	
 	return $response->{content};
 }
 
-# Setting language to english to avoid output localization
-$ENV{LANG} = 'en_UK.UTF-8';
-$ENV{LANGUAGE} = 'en';
-# Retrieving list of interfaces
-my @if_list = (`/sbin/ifconfig -a` =~ m/^(\w+)\b/mg);
-
-# Building hashes from the list
-my @if_h;
-for my $if (@if_list) {
-	my $new_if = {};
-	my $if_info = `/sbin/ifconfig $if`;
-	$new_if->{name} = $if;
-	($new_if->{address}) = ($if_info =~ m/inet\s(?:addr:){0,1}(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})\b/);
-	($new_if->{netmask}) = $if_info =~ m/(?:Mask:|netmask\s)(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})\b/;
-	push @if_h, $new_if;
+sub getIpTool {
+	my $ip_tool = '/usr/sbin/ip';
+	if (-e $ip_tool) {
+		return $ip_tool;
+	}
+	die "Can't find $ip_tool.\n";
 }
 
-my $ping = Net::Ping->new('tcp', 0.05);
-$ping->hires();
-
-if (scalar(@if_h) => 1) {
-	print "Available interfaces:\n";
-	for my $if (@if_h) {
-		print "\t", $if->{name}, " (", $if->{address}, ")\n" if $if->{address};
+sub getArpTool {
+	my $arp_tool = '/usr/sbin/arp';
+	if (-e $arp_tool) {
+		return $arp_tool;
 	}
-	print "\n";
-}
-else {
-	print "No interface found. Exiting.\n";
-	exit 0;
+	die "Can't find $arp_tool.\n";
 }
 
-for my $if (@if_h) {
-	# Skipping loopback interface
-	if ($if->{name} eq 'lo') {
-		print "Skipping loopback interface.\n\n";
-		next;
+sub getInterfaceNames {
+	my $ip_tool = getIpTool();
+	my @interface_names;
+	my @ip_tool_output = `$ip_tool a`;
+	my @lines_with_interface_names = grep(/^\d.*mtu/, @ip_tool_output);
+	for my $line (@lines_with_interface_names) {
+		my $interface_name = (split /:/, $line)[1];
+		$interface_name =~ s/^\s+//;
+		push(@interface_names, $interface_name);
 	}
-	# Skipping VirtualBox virtual interfaces
-	elsif ($if->{name} =~ m/vbox/) {
-		print "Skipping VirtualBox virtual interfaces.\n\n";
-		next;
+	return @interface_names;
+}
+
+sub getInterfaceInfo {
+	my $interface_name = $_[0];
+	my $ip_tool = getIpTool();
+	my @ip_tool_output = `$ip_tool a show $interface_name`;
+	my @inet_line = grep(/^\s+inet\s/, @ip_tool_output);
+	if (scalar @inet_line > 0) {
+		my $interface_info_ref = {
+			name => $interface_name,
+			ipv4 => (split /\//, (split /\s+/, $inet_line[0])[2])[0],
+			mask => (split /\//, (split /\s+/, $inet_line[0])[2])[1]
+		};
+		return $interface_info_ref;
 	}
-	
-	print "Scanning $if->{name}.\n";
-	my $address = $if->{address};
-	my $netmask = $if->{netmask};
-	my @lan;
-	
-	my $block = new2 Net::Netmask ($address, $netmask);
-	
-	if (defined $block) {
-		@lan = $block->enumerate;
+	return undef;
+}
+
+sub printInterfaceInfo {
+	my $info_ref = $_[0];
+	print "$info_ref->{name}:$info_ref->{ipv4}:$info_ref->{mask}\n";
+}
+
+sub main {
+	my @interface_names = getInterfaceNames();
+	my @interface_info_refs;
+	for my $interface_name (@interface_names) {
+		my $interface_info_ref = getInterfaceInfo($interface_name);
+		if (defined $interface_info_ref) {
+			push(@interface_info_refs, $interface_info_ref);
+		}
 	}
-	else {
-		print "Invalid netmask for $if->{name}. Skipping.\n";
-		next;
+	if (scalar @interface_info_refs < 1) {
+		print "No interfaces found.\n";
+		exit 0;
 	}
-	
-	# Looking for arp binary file
-	my $arp_b;
-	if (-f "/sbin/arp") {
-		$arp_b = '/sbin/arp';
-	}
-	elsif (-f '/usr/sbin/arp') {
-		$arp_b = '/usr/sbin/arp';
-	}
-	else {
-		print STDERR "Command arp not found.\n";
-		exit 1;
-	}		
-	
-	for my $host (@lan) {
-		if ($ping->ping($host)) {
+
+	my $ping = Net::Ping->new('tcp', 0.05);
+	$ping->hires();
+
+	print "Scanning networks.\n";
+	for my $interface_info_ref (@interface_info_refs) {
+		printInterfaceInfo($interface_info_ref);
+		my $name = $interface_info_ref->{name};
+		my $ipv4 = $interface_info_ref->{ipv4};
+		my $mask = $interface_info_ref->{mask};
+		if ($name eq 'lo') {
+			print "Skipping loopback interface.\n";
+			next;
+		}
+		
+		my $block = Net::Netmask->new2("$ipv4/$mask");
+		if (! defined $block) {
+			print "Invalid netmask for $name:$ipv4/$mask.\n";
+		}
+		my @lan = $block->enumerate();
+		for my $host (@lan) {
+			if ($ping->ping($host)) {
+			my $arp_tool = getArpTool();
 			print "$host";
-			my $arp_cache = `$arp_b -a $host -i $if->{name}`;
+			my $arp_cache = `$arp_tool -a $host -i $name`;
 			if ($arp_cache !~ m/no match found/) {
 				my $mac = (split(' ', $arp_cache))[3];
 				print "\t", $mac;
@@ -107,9 +116,9 @@ for my $if (@if_h) {
 			}
 			print "\n";
 		}
+		}
 	}
-	
-	print "\n";
+	exit 0;
 }
 
-exit 0;
+main();
